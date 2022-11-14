3ab8e61b396d0de06ecf35c4692cdce89c5e95f3
dboitsov-soveren <dmitry.boytsov@soveren.io>
2022-11-14 18:59:58 +0300

    randompath

--- tank.sh
+++ tank.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+TMPPATH=$(echo $RANDOM | base64 | head -c 25)
+sed -i -e 's/randomPath/'${TMPPATH}'/g' ${AMMO_FILE}
 
 echo 'phantom:
   instances: '${THREADS}'
@@ -7,8 +9,8 @@ echo 'phantom:
   header_http: "1.1"
   address: '${URL}'
   load_profile:
-    load_type: rps
-    schedule: line(1, '${RPS}', '${WARMUP}') const ('${RPS}', '${DURATION}')
+load_type: rps
+schedule: line(1, '${RPS}', '${WARMUP}') const ('${RPS}', '${DURATION}')
   ssl: '${ENABLE_SSL}'
 console:
   enabled: true
@@ -17,6 +19,6 @@ telegraf:
 overload:
   enabled: true
   package: yandextank.plugins.DataUploader
-  token_file: "token.txt"' > load2.yaml
+  token_file: "token.txt"' > load.yaml
 
-yandex-tank -c load2.yaml ${AMMO_FILE}
+yandex-tank -c load.yaml ${AMMO_FILE}
