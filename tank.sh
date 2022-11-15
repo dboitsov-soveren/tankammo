d51a3770ae53068fb54d3a0ca8b589caff7e6754
Dmitrii B <106156261+dboitsov-soveren@users.noreply.github.com>
2022-11-15 13:54:04 +0300

    Update tank.sh

--- tank.sh
+++ tank.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 TMPPATH=$(echo $RANDOM | base64 | head -c 25)
+echo TMPPATH
+
 sed -i -e 's/randomPath/'${TMPPATH}'/g' ${AMMO_FILE}
 
 echo 'phantom:
@@ -10,8 +12,8 @@ echo 'phantom:
   header_http: "1.1"
   address: '${URL}'
   load_profile:
-    load_type: rps
-    schedule: line(1, '${RPS}', '${WARMUP}') const ('${RPS}', '${DURATION}')
+load_type: rps
+schedule: line(1, '${RPS}', '${WARMUP}') const ('${RPS}', '${DURATION}')
   ssl: '${ENABLE_SSL}'
 console:
   enabled: true
