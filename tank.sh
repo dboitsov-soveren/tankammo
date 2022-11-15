c7060a3887e7782605c46cd1e953cdb3e4b7d829
Dmitrii B <106156261+dboitsov-soveren@users.noreply.github.com>
2022-11-15 19:26:39 +0300

    Update tank.sh

--- tank.sh
+++ tank.sh
@@ -7,21 +7,16 @@ sed -i -e 's/randomPath/'${TMPPATH}'/g' ${AMMO_FILE}
 
 echo 'phantom:
   instances: '${THREADS}'
-  timeout: 10s
-  ammo_type: uripost
   header_http: "1.1"
   address: '${URL}'
   load_profile:
-load_type: rps
-schedule: line(1, '${RPS}', '${WARMUP}') const ('${RPS}', '${DURATION}')
+    load_type: rps
+    schedule: line(1, '${RPS}', '${WARMUP}') const ('${RPS}', '${DURATION}')
   ssl: '${ENABLE_SSL}'
 console:
   enabled: true
-telegraf:
-  enabled: false
 overload:
   enabled: true
   package: yandextank.plugins.DataUploader
-  token_file: "token.txt"' > load.yaml
-
-yandex-tank -c load.yaml ${AMMO_FILE}
+  token_file: "token.txt"' > load2.yaml
+yandex-tank -c load2.yaml ${AMMO_FILE}
