b4e78883b505c90800cb14fab61a9e0d001062b7
dboitsov-soveren <dmitry.boytsov@soveren.io>
2023-05-19 07:26:06 +0300

    Temp multi-targets

--- startLoad.sh
+++ startLoad.sh
@@ -11,11 +11,22 @@ echo 'phantom:
   timeout: 10s
   ammo_type: uripost
   header_http: "1.1"
-  address: '${URL}'
+  address: a9f9684208c7946e09ace815dcf598df-1b9147536e3097a4.elb.eu-west-1.amazonaws.com
   load_profile:
     load_type: rps
     schedule: line(1, '${RPS}', '${WARMUP}') const ('${RPS}', '${DURATION}')
   ssl: '${ENABLE_SSL}'
+  multi:
+    - address: a197f77c4251845adab0de156debbf78-328989716.eu-west-1.elb.amazonaws.com:9000
+      load_profile:
+        load_type: rps
+        schedule: schedule: line(1, '${RPS}', '${WARMUP}') const ('${RPS}', '${DURATION}')
+      ssl: '${ENABLE_SSL}'
+    - address: a272cf379cf0f430eaf2b24e176ccefa-d8a07963be221813.elb.eu-west-1.amazonaws.com:9000
+      load_profile:
+        load_type: rps
+        schedule: schedule: line(1, '${RPS}', '${WARMUP}') const ('${RPS}', '${DURATION}')
+      ssl: '${ENABLE_SSL}'
 console:
   enabled: true
 telegraf:
